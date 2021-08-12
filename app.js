40562222d60f51ad3e41d1d62412b4c6c52bb8b1
BarnabeLB <lebelbarnabe@gmail.com>
2021-08-12 11:20:14 +0200

    inscreasing security using bcrypt and its round-salting-hash methods

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ require('dotenv').config();                                                     
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");                                                           // bcrypt permet un hashage avec des rounds de salt (c'est à dire que le hash se retrouve "salé" un certain nombre de fois de suite)
+const saltRounds = 10;                                                                      // nombre de round de salt
 
 
 const app = express();
@@ -40,32 +41,41 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),                                                  // on stock dans la database un hash du password entré par l'user
-  });
-
-  newUser.save(function (err) {                                                   
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+  
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {                      // la méthode .hash() transforme l'input de l'user et retourne un hash avec un certain nombre de round de salt
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,                                                                   // on stock dans la database le hash retourné par la méthode .hash() de bcrypt
+    });
+  
+    newUser.save(function (err) {                                                   
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+  })
+  
 });
 
 app.post("/login", function (req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);                                              // on transforme l'input grace à la méthode md5()
+  const password = req.body.password;                                             
 
   User.findOne({ email: username},function (err, foundUser) {                      
       if (err) {
         console.log(err);
       } else {
         if (foundUser) {
-          if (foundUser.password === password) {                                        // si les hashs correspondent
-            res.render("secrets");                                                      // on donne accès à la page secrète.
-          }
+          bcrypt.compare(password, foundUser.password, function(err, result) {        // la méthode .compare() l'input de l'user et le hash stocké dans la database. La méthode retourne un booléén
+            if (result === true) {                                                    // si, ça valeur est true, c'est que le hash de l'input correspond au hash enregistré
+              res.render("secrets");                                                  // on donne donc accès à la page secrète.
+            } else {
+              console.log(err);
+            }
+          });
+          
         }
       }
     }
